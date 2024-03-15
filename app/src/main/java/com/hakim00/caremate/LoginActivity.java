fbbfb97d3ad62519454e9c4492b9f5490cca13c5
Hakim Rahimi Safi <hakimsafi402@gmail.com>
2024-03-16 00:34:08 +0430

    updated

--- app/src/main/java/com/hakim00/caremate/LoginActivity.java
+++ app/src/main/java/com/hakim00/caremate/LoginActivity.java
@@ -2,13 +2,65 @@ package com.hakim00.caremate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                Database db = new Database(LoginActivity.this, "caremate", null, 1);
+
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplication(),"Please Fill All The Field", Toast.LENGTH_SHORT).show();
+                } else{
+                    if(db.login(username, password)==1){
+                        Toast.makeText(getApplication(),"login Success", Toast.LENGTH_SHORT).show();
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", username);
+
+                        //to start our data with key and value
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+
+                    }else{
+                        Toast.makeText(getApplication(),"Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+       tv.setOnClickListener(new View.OnClickListener() {
+           @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+           }
+       });
     }
 }
--- app/src/main/java/com/hakim00/caremate/RegisterActivity.java
+++ app/src/main/java/com/hakim00/caremate/RegisterActivity.java
@@ -2,13 +2,96 @@ package com.hakim00.caremate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername, edEmail, edPassword, edRetypePassword;
+    Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edRetypePassword = findViewById(R.id.editTextRegRetypePassword);
+        btn = findViewById(R.id.buttonReg);
+        tv = findViewById(R.id.textViewRegLogin);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String email = edEmail.getText().toString();
+                String password = edPassword.getText().toString();
+                String retypePassword = edRetypePassword.getText().toString();
+                Database db = new Database(getApplicationContext(), "caremate", null, 1);
+
+                if (username.length() == 0 || email.length() == 0 || password.length() == 0 || retypePassword.length() == 0) {
+                    Toast.makeText(getApplication(), "Please Fill All The Field", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (password.compareTo(retypePassword) == 0) {
+                        if (isValid(password)) {
+                            db.register(username, email, password);
+                            Toast.makeText(getApplication(), "Register Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+                        } else {
+                            Toast.makeText(getApplication(), "Password must contains 8 character having digit number and symbol", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(getApplication(), "Password Didn't Matched", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
+
+
+     public static boolean isValid(String passwordhere) {
+         int f1 = 0, f2 = 0, f3 = 0;
+         if (passwordhere.length() < 8) {
+             return false;
+         } else {
+             for (int p = 0; p < passwordhere.length(); p++) {
+                 if (Character.isLetter(passwordhere.charAt(p))) {
+                     f1 = 1;
+                 }
+             }
+             for (int r = 0; r < passwordhere.length(); r++) {
+                 if (Character.isDigit(passwordhere.charAt(r))) {
+                     f2 = 1;
+                 }
+             }
+             for (int s = 0; s < passwordhere.length(); s++) {
+                 char c = passwordhere.charAt(s);
+                 if (c >= 33 && c <= 46 || c == 64) {
+                     f3 = 1;
+                 }
+             }
+             if (f1 == 1 & f2 == 1 & f3 == 1) {
+                 return true;
+             }
+             return false;
+         }
+     }
 }
